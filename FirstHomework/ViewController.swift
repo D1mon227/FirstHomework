317bd8ebb9a3c62f56d6448aaa68fa09b6dc9797
Dmitry <d1mon227@yandex.ru>
2022-11-29 13:11:12 +0200

    Initial Commit

---
+++ FirstHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstHomework
+//
+//  Created by Dmitry Medvedev on 29.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
