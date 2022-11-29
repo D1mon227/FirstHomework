6f5f44e8a49979c76a80cc2cb90ce47355e73af3
Dmitry <d1mon227@yandex.ru>
2022-11-29 14:40:37 +0200

    Домашнее задание № 1. Решение задачки Счетчик.

--- FirstHomework/ViewController.swift
+++ FirstHomework/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var count: Int = 0
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var clickButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterLabel.backgroundColor = .blue
+        counterLabel.textColor = .systemYellow
+        clickButton.backgroundColor = .red
+        clickButton.tintColor = .green
+        
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func clickAndCount(_ sender: Any) {
+        count += 1
+        counterLabel.text = "Значение счетчика: \(count)"
+    }
+    
+    
 }
 
